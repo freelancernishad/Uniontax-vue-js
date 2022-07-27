3148525417d11c6b4bac6b9a7167d31091005e1d
Softlabltd.com <freelancernishad123@gmail.com>
2022-07-27 13:44:50 +0600

    asset update

--- resources/views/frontlayout.blade.php
+++ resources/views/frontlayout.blade.php
@@ -5,17 +5,17 @@
     <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>:: Welcome to Upsheba ::</title>
-    <link href="https://kit-pro.fontawesome.com/releases/v5.15.2/css/pro.min.css" rel="stylesheet">
+    <link href="https://kit-pro.fontawesome.com/releases/v5.15.2/css/pro.min.css" rel="stylesheet" onload="if(media!=='all')media='all'">
     <!-- CSS only -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/bangladesh-govt.png') }}" />
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
-    <script src="{{ asset('assets/js/jquery-3.2.1.slim.min.js') }}"></script>
-    <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+    {{-- <script src="{{ asset('assets/js/jquery-3.2.1.slim.min.js') }}"></script> --}}
+    {{-- <link href="https://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css"> --}}
     {{-- <link href="css/style.css" rel="stylesheet" type="text/css" /> --}}
-    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
-    <script type="text/javascript" src="{{ asset('assets/js/jquery.rcbrand.js') }}"></script>
+    {{-- <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script> --}}
+    {{-- <script type="text/javascript" src="{{ asset('assets/js/jquery.rcbrand.js') }}"></script> --}}
 
 	<link href="https://fonts.maateen.me/kalpurush/font.css" rel="stylesheet">
 
@@ -74,10 +74,10 @@ div#exampleModal {
 
     </div>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+{{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script> --}}
 
 
-<script type="text/javascript" src="{{ asset('slider/js/jquery.bbslider.min.js') }}"></script>
+{{-- <script type="text/javascript" src="{{ asset('slider/js/jquery.bbslider.min.js') }}"></script> --}}
 
 {{-- <script type="text/javascript">
   $(document).ready(function () {
