46787f9ef9626279320404dddba50cefa3d850cf
freelancernishad <freelancernishad123@gmail.com>
2022-08-07 00:37:34 +0600

    uuu

--- app/Http/Controllers/SonodController.php
+++ app/Http/Controllers/SonodController.php
@@ -343,8 +343,8 @@ class SonodController extends Controller
             $unioun_name = $r->unioun_name;
               $sonod_name = $r->sonod_name;
             // return  $Insertdata['sonod_Id'] = $this->allsonodId($unioun_name,$sonod_name);
-            return $Insertdata['sonod_Id'] =  $this->allsonodId($unioun_name,$sonod_name);
-            $sonod =   sonod::create($Insertdata);
+             $Insertdata['sonod_Id'] =  $this->allsonodId($unioun_name,$sonod_name);
+            return $sonod =   sonod::create($Insertdata);
 
             if($stutus=='Pending'){
 
