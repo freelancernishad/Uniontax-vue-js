fa4ac576eb69c75a3cd5b5bc0b8c9c04cbfd901e
Softlabltd.com <freelancernishad123@gmail.com>
2023-01-30 00:30:58 +0600

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -69,7 +69,7 @@ li.page-item.active button {
 
 
 
-<script src="{{ asset('js/backend.js?ver=1.4.3301') }}"></script>
+<script src="{{ asset('js/backend.js?ver=1.4.3302') }}"></script>
 
 
 
--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,7 @@ Route::group([
 
 });
 
-
+Route::post('nagorik/seba/insert',[SonodController::class, 'sonod_submit']);
 
 // country api
 Route::get('/getdivisions', [countryApiController::class,'getdivisions']);
@@ -128,7 +128,7 @@ Route::get('sonod/list',[SonodController::class,'index']);
 Route::post('role/assign',[authController::class,'roleAssign']);
 
 Route::get('sonod/single/{id}',[SonodController::class, 'singlesonod']);
-Route::post('get/sonods/submit',[SonodController::class, 'sonod_submit']);
+
 Route::post('sonod/update',[SonodController::class, 'sonod_update']);
 
 Route::get('sonod/delete/{id}',[SonodController::class, 'sonod_delete']);
