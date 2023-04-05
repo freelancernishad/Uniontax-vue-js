bd68aac692ad7983694a06f36cd4a5ef80c0a24d
Softlabltd.com <freelancernishad123@gmail.com>
2023-04-05 11:26:22 +0600

    update

--- app/Http/Controllers/SonodController.php
+++ app/Http/Controllers/SonodController.php
@@ -2237,6 +2237,7 @@ $TaxInvoice = Payment::where('sonodId',$row->id)->latest()->first();
 $C_color = '#7230A0';
 $C_size = '18px';
 $color = 'black';
+$style = '';
 if($row->unioun_name=='dhamor'){
     $C_color = '#5c1caa';
     $C_size = '20px';
@@ -2244,7 +2245,17 @@ if($row->unioun_name=='dhamor'){
 }
 
 
-            $ccc = '<img width="170px"  src="' . base64($row->chaireman_sign) . '"><br/>
+if($row->unioun_name=='toria'){
+    $C_color = '#5c1caa';
+    $style = "
+
+    margin-bottom: -33px;
+margin-left: 83px;
+    ";
+
+}
+
+            $ccc = '<img width="170px"  style="'.$style.'" src="' . base64($row->chaireman_sign) . '"><br/>
                               <b><span style="color:'.$C_color.';font-size:'.$C_size.';">' . $row->chaireman_name . '</span> <br />
                                       </b><span style="font-size:16px;">চেয়ারম্যান</span><br />';
 
