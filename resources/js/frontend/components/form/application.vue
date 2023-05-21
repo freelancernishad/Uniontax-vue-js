b3c1b26d081d7038b6f7fcdd2c2e944cddd5e71f
Softlabltd.com <freelancernishad123@gmail.com>
2023-05-21 23:29:23 +0600

    update

--- resources/js/frontend/components/form/application.vue
+++ resources/js/frontend/components/form/application.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <Applicationform v-if="!getunionInfos.nidService"/>
+        <Applicationform v-if="getunionInfos.nidService==0"/>
         <ApplicationformwitnNid v-else/>
     </div>
 </template>
